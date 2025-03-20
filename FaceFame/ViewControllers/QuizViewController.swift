a6efd8661629033dfdf80f7fa4559c300e56dfb2
Alastafew <alexastafew@gmail.com>
2025-03-20 23:40:33 +0300

    Add 5 questions

--- FaceFame/ViewControllers/QuizViewController.swift
+++ FaceFame/ViewControllers/QuizViewController.swift
@@ -14,6 +14,7 @@ final class QuizViewController: UIViewController {
     // MARK: - IBOutlets
     @IBOutlet var ActorImageView: UIImageView!
     @IBOutlet var answerButtons: [UIButton]!
+    @IBOutlet var questionProgressView: UIProgressView!
     
     //MARK: - Private properties
     private var questionIndex = 0
@@ -36,19 +37,24 @@ final class QuizViewController: UIViewController {
     }
     
     @IBAction func answerButtonAction(_ sender: Any) {
+        questionIndex += 1
+        //questionProgressView += 0.2
+        if questionIndex == questions.count {
+            showAlert(withTitle: "Результат", andMessage: "Ваш результат: 10 очков")
+            questionIndex = 0
+        }
+        updateUI()
     }
     
     // MARK: - Private methods
     private func updateUI() {
         title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
-        print(questions[questionIndex].currectAnswer)
+        //print(questions[questionIndex].currectAnswer)
         ActorImageView.image = questions[questionIndex].imageActor
-        //ActorImageView.image = UIImage(named: questions[questionIndex].imageActor)
         
         for (buttonIndex, button) in answerButtons.enumerated() {
             if buttonIndex < questions.count {
                 button.setTitle(questions[questionIndex].answers[buttonIndex], for: .normal)
-                button.titleLabel?.textAlignment = .center
             }
         }
     }
