0396961046f3127fc79ab6170035c562c1f23a2c
Algense17 <leonv909@gmail.com>
2025-03-21 19:36:33 +0300

    ver 6

--- FaceFame/ViewControllers/QuizViewController.swift
+++ FaceFame/ViewControllers/QuizViewController.swift
@@ -14,6 +14,7 @@ final class QuizViewController: UIViewController {
     // MARK: - IBOutlets
     @IBOutlet private var ActorImageView: UIImageView!
     @IBOutlet private var answerButtons: [UIButton]!
+    @IBOutlet var progressBarView: UIProgressView!
     
     //MARK: - Public properties
     var currectAnswersCount = 0
@@ -23,11 +24,33 @@ final class QuizViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+            let titleLabel = UILabel()
+            titleLabel.text = "FaceFame"
+            titleLabel.textAlignment = .center
+            titleLabel.font = UIFont.boldSystemFont(ofSize: 40)
+        titleLabel.textColor = UIColor(
+            red: 242/255,
+            green: 195/255,
+            blue: 130/255,
+            alpha: 1
+        )
+            titleLabel.sizeToFit()
+            self.navigationItem.titleView = titleLabel
+        
         updateUI()
+        view.addVerticalGradientLayer()
         
         questions.forEach{
             print($0.currectAnswer)
         }
+            answerButtons.forEach{ button in
+                button.layer.borderColor = UIColor.white.cgColor
+                button.layer.borderWidth = 2
+                button.layer.cornerRadius = button.frame.height / 2
+            }
+        
+        ActorImageView.layer.cornerRadius = ActorImageView.frame.height / 5
     }
     
     private func showAlert(withTitle title: String, andMessage message: String) {
@@ -35,6 +58,12 @@ final class QuizViewController: UIViewController {
         let okAction = UIAlertAction(title: "Ok", style: .default)
         alert.addAction(okAction)
         present(alert, animated: true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let resultVC = segue.destination as? QuizResutlsViewController {
+            // что-то передадим
+        }
     }
     
     // MARK: - IBActions
@@ -47,16 +76,52 @@ final class QuizViewController: UIViewController {
         if currentAnswer == questions[questionIndex].currectAnswer {
             print("угадал")
             currectAnswersCount += 1
+            
+            UIView.animate(withDuration: 0.3, animations: {
+                sender.backgroundColor = .green
+            }) { _ in
+                UIView.animate(withDuration: 0.3, animations: {
+                    sender.backgroundColor = .clear
+                }, completion: { _ in
+                    self.nextQuestion()
+                })
+            }
+            
         } else {
             print("найн")
+            
+            for button in answerButtons {
+                if button.currentTitle == questions[questionIndex].currectAnswer {
+                    UIView.animate(withDuration: 0.3, animations: {
+                        button.backgroundColor = .green
+                    }) { _ in
+                        UIView.animate(withDuration: 0.3, animations: {
+                            button.backgroundColor = .clear
+                        }, completion: { _ in
+                        })
+                    }
+                }
+            }
+            
+            UIView.animate(withDuration: 0.3, animations: {
+                sender.backgroundColor = .red
+            }) { _ in
+                UIView.animate(withDuration: 0.3, animations: {
+                    sender.backgroundColor = .clear
+                }, completion: { _ in
+                    self.nextQuestion()
+                })
+            }
         }
-        nextQuestion()
         
     }
     
     // MARK: - Private methods
     private func updateUI() {
-        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+ 
+        let currentProgress = Float(questionIndex) / Float(questions.count)
+        progressBarView.setProgress(currentProgress, animated: true)
+
         //прнты
         
         print("текущий актер:\(questions[questionIndex].currectAnswer)")
@@ -81,7 +146,7 @@ final class QuizViewController: UIViewController {
             updateUI()
             return
         }
-        // переход по сигвею
+        performSegue(withIdentifier: "showResult", sender: nil)
     }
 }
 
